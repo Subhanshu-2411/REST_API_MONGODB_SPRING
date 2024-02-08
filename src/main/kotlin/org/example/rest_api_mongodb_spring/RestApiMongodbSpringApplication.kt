3741c502b47cd24a1e4290f61ec881c60ed521af
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2024-02-09 00:21:13 +0530

    Initial Commit

---
+++ src/main/kotlin/org/example/rest_api_mongodb_spring/RestApiMongodbSpringApplication.kt
@@ -0,0 +1,11 @@
+package org.example.rest_api_mongodb_spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApiMongodbSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestApiMongodbSpringApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/rest_api_mongodb_spring/RestApiMongodbSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.rest_api_mongodb_spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiMongodbSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
